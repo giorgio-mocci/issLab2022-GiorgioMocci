0ca2cc4ecf761dcaa474b291a493a2d5fad7c92b
giorgio-mocci <68387781+giorgio-mocci@users.noreply.github.com>
2022-03-04 09:48:10 +0100

    SonarProject

---
+++ SonarProject/Raspberry software/ledBlink.sh
@@ -0,0 +1,18 @@
+# -------------------------------------------------------------
+# led25GpioTurnOn.sh
+# Key-point: we can manage a device connected on a GPIO pin by
+# using the GPIO shell library. 
+# The pin 25 is physical 22 and Wpi 6.
+#	sudo bash led25GpioTurnOn.sh
+# -------------------------------------------------------------
+gpio mode 6 out #
+
+for i in {1..3}
+do
+gpio write 6 1 #
+sleep 0.02
+gpio write 6 0 #
+sleep 0.02
+done
+
+
